187b0862c7fd9fa99223cc05b90e695f02e21a43
Nikolas Turunen <nikoltur@gmail.com>
2018-11-09 11:53:34 +0200

    Added styling for login form

---
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+import './LoginForm.css';
+
+import { Form, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+
+import { onLogin, onRegister } from '../../actions/loginActions';
+
+class LoginForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.defaultState = {
+            username: "",
+            password: ""
+        };
+
+        this.state = this.defaultState;
+    }
+
+    onChange = (event) => {
+        if (event.target.name === "username") {
+            this.setState({
+                username: event.target.value
+            });
+        }
+
+        if (event.target.name === "password") {
+            this.setState({
+                password: event.target.value
+            });
+        }
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        let user = {
+            username: this.state.username,
+            password: this.state.password
+        };
+
+        if (event.target.name === "register") {
+            this.props.dispatch(onRegister(user));
+        } else {
+            this.props.dispatch(onLogin(user));
+        }
+
+        this.setState(this.defaultState);
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <div className="login-title">Login</div>
+                <Form>
+                    <Form.Field>
+                        <label className="login-label">Username: </label>
+                        <input
+                            className="normal-input"
+                            type="text"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.onChange}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label className="login-label">Password: </label>
+                        <input
+                            className="normal-input"
+                            type="password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.onChange}
+                        />
+                    </Form.Field>
+
+                    <Button className="normal-button login-button" onClick={this.onSubmit} name="login">Login</Button>
+                    <Button className="normal-button login-button" onClick={this.onSubmit} name="register">Register</Button>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default connect()(LoginForm);
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import LoginForm from './LoginForm';
+import LoginForm from './LoginForm/LoginForm';
 import Home from './Home/Home';
 
 export default class Main extends React.Component {
